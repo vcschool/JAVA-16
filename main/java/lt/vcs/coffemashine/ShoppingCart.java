d04c394d40f12020d1043a2bfa8758c4c5702e47
Audrius <audriusgurskis@gmail.com>
2017-12-18 00:08:46 +0200

    Initiated project

---
+++ main/java/lt/vcs/coffemashine/ShoppingCart.java
@@ -0,0 +1,42 @@
+package lt.vcs.coffemashine;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ShoppingCart {
+
+    List<String> items;
+
+    ShoppingCart() {
+        items = new ArrayList<>();
+    }
+
+    void add(String item) {
+        items.add(item);
+    }
+
+    int getTotalItems() {
+        return items.size();
+    }
+
+    Boolean doesContain(String itemName) {
+        return items.contains(itemName);
+    }
+
+    Double checkout() {
+        double total = 0;
+        for (String item : items) {
+            if (item.equals("Bread")) {
+                total += 1;
+            } else if (item.equals("Milk")) {
+                total += 0.6;
+            } else if (item.equals("Banana")) {
+                total += 0.4;
+            } else
+                total += 0.1;
+        }
+        return total;
+    }
+
+}
